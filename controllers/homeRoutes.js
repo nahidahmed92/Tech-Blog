0a0e63bb2503788b118cda3187ec37b1c90ddc89
Nahid Ahmed <npahmed92@gmail.com>
2024-04-15 23:38:36 -0400

    find blog post by id and serialize the data

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -26,4 +26,26 @@ router.get('/', async (req, res) => {
   }
 });
 
+router.get('/blogs/:id', async (req, res) => {
+  try {
+    const blogPostData = await BlogPost.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          attributes: ['name'],
+        },
+      ],
+    });
+
+    const blogs = blogPostData.get({ plain: true });
+
+    res.render('blogs', {
+      ...blogs,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
