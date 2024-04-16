53429ff39a425fe3c52868d768e70c90d0faf321
Nahid Ahmed <npahmed92@gmail.com>
2024-04-15 23:35:13 -0400

    find all blog post and serialize the data

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,29 @@
+const router = require('express').Router();
+const { BlogPost, User } = require('../models');
+
+router.get('/', async (req, res) => {
+  try {
+    // Get all projects and JOIN with user data
+    const blogPostData = await BlogPost.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ['name'],
+        },
+      ],
+    });
+
+    // Serialize data so the template can read it
+    const blogs = blogPostData.map((blog) => blog.get({ plain: true }));
+
+    // Pass serialized data and session flag into template
+    res.render('homepage', {
+      blogs,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
