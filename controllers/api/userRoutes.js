8f79adfb19bb20f698eef92bebad26ecd40394ba
Nahid Ahmed <npahmed92@gmail.com>
2024-04-15 23:13:15 -0400

    add route for users when logged in

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -16,4 +16,34 @@ router.post('/', async (req, res) => {
   }
 });
 
+router.post('/login', async (req, res) => {
+  try {
+    const userData = await User.findOne({
+      where: {
+        email: req.body.email,
+      },
+    });
+    if (!userData) {
+      res.status(400).json({ message: 'Email or password is incorrect' });
+      return;
+    }
+    const validPassword = userData.checkPassword(req.body.password);
+    if (!validPassword) {
+      res.status(400).json({ message: 'Email or password is incorrect' });
+      return;
+    }
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
+      res
+        .status(200)
+        .json({ loggedIn: true, message: 'You are now logged in!' });
+      console.log('Logged in!');
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
