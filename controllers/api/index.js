b51d3a974902813d89b500ca8c203f4481b7c262
Nahid Ahmed <npahmed92@gmail.com>
2024-04-21 03:28:46 -0400

    add dashboard route

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes.js');
 const blogPostRoutes = require('./blogPostRoutes.js');
+const dashboardRoutes = require('./dashboardRoutes.js');
 
 router.use('/users', userRoutes);
 // router.use('/blogposts', blogPostRoutes);
+router.use('/dashboard', dashboardRoutes);
 
 module.exports = router;
