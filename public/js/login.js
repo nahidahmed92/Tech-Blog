f3ea368563619883f0b04ede97ea9175479520ba
Nahid Ahmed <npahmed92@gmail.com>
2024-04-21 04:34:08 -0400

    fetch user input and login user

--- public/js/login.js
+++ public/js/login.js
@@ -4,6 +4,15 @@ const loginFormHandler = async (event) => {
   // Collect values from the login form
   const username = document.querySelector('#login-username').value.trim();
   const password = document.querySelector('#login-password').value.trim();
+
+  if (username && password) {
+    // Send a POST request to the API endpoint
+    const response = await fetch('/api/users/login', {
+      method: 'POST',
+      body: JSON.stringify({ username, password }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+  }
 };
 
 document
