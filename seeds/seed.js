0efd48a8fbbba8de0e204c881bf7dbef696bb770
Nahid Ahmed <npahmed92@gmail.com>
2024-04-21 23:49:52 -0400

    create seed comments

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,8 +1,9 @@
 const sequelize = require('../config/connection.js');
-const { User, BlogPost } = require('../models');
+const { User, BlogPost, Comment } = require('../models');
 
 const userData = require('./userData.json');
 const blogPostData = require('./blogPostData.json');
+const commentData = require('./commentData.json');
 
 const seedDB = async () => {
   await sequelize.sync({ force: true });
@@ -12,10 +13,32 @@ const seedDB = async () => {
     returning: true,
   });
 
+  const createdBlogPosts = [];
+
   for (const blogPost of blogPostData) {
-    await BlogPost.create({
+    const createdBlogPost = await BlogPost.create({
       ...blogPost,
-      user_id: users[Math.floor(Math.random() * users.length)].id,
+      userId: users[Math.floor(Math.random() * users.length)].id,
+    });
+    createdBlogPosts.push(createdBlogPost);
+  }
+
+  // Create Comments at random
+  for (const comment of commentData) {
+    // Get a random users `id`
+    const { id: randomUserId } =
+      users[Math.floor(Math.random() * users.length)];
+    const randomBlogPost =
+      createdBlogPosts[Math.floor(Math.random() * createdBlogPosts.length)];
+    const randomBlogPostId = randomBlogPost.id;
+
+    // Create a new comment with random `userId` and `blogId` values
+    await Comment.create({
+      ...comment,
+      userId: randomUserId,
+      blogId: randomBlogPostId,
+    }).catch((err) => {
+      console.log(err);
     });
   }
   process.exit(0);
