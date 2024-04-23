078712eb6dbad16a90143ec9f67f2431bc357936
Nahid Ahmed <npahmed92@gmail.com>
2024-04-22 21:22:56 -0400

    create blog handler

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -7,12 +7,34 @@ const dbFormHandler = async (event) => {
 
   if (dbContainer.style.display === 'none') {
     dbContainer.style.display = 'block';
+    dbContainer.scrollIntoView({ behavior: 'smooth' });
   } else if (createBtn) {
     dbContainer.style.display = 'none';
   }
 };
-// const dbTitle = document.querySelector('#dashboard-title').value.trim();
-// const dbContent = document.querySelector('#dashboard-content').value.trim();
+
+const createBlogHandler = async (event) => {
+  event.preventDefault();
+  const title = document.querySelector('#dashboard-title').value.trim();
+  const content = document.querySelector('#dashboard-content').value.trim();
+
+  try {
+    const response = await fetch('api/dashboard', {
+      method: 'POST',
+      body: JSON.stringify({ title, content }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+      window.location.href = '/dashboard';
+    } else {
+      console.error('Error submitting comment:', response.statusText);
+    }
+  } catch (err) {
+    console.error(err);
+  }
+};
 
 newPostBtn.addEventListener('click', dbFormHandler);
 createBtn.addEventListener('click', dbFormHandler);
+createBtn.addEventListener('click', createBlogHandler);
